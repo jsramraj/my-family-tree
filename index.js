23e2bbc047d29c058e134396851464ab48cb2ec7
Ramaraj Thangapandi <ramaraj_thangapandi@trimble.com>
2023-05-27 14:53:44 +0530

    add all files

---
+++ index.js
@@ -0,0 +1,52 @@
+//JavaScript
+(function (window, document, undefined) {
+  // code that should be taken care of right away
+
+  window.onload = init;
+
+  var family;
+
+  function getFamilyMembers() {
+    var familyMembers = [];
+
+    var data = familyData();
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(data, "text/xml");
+    var nodes = xmlDoc.getElementsByTagName("node");
+    console.log(xmlDoc);
+    for (i = 0; i < nodes.length; i++) {
+      familyMembers.push({
+        id: nodes[i].getAttribute("id"),
+        pids: nodes[i].getAttribute("pids"),
+        mid: nodes[i].getAttribute("mid"),
+        fid: nodes[i].getAttribute("fid"),
+        name: nodes[i].getAttribute("name"),
+        gender: nodes[i].getAttribute("gender"),
+      });
+    }
+    return familyMembers;
+  }
+
+  function init() {
+    family = new FamilyTree(document.getElementById("tree"), {
+      menu: {
+        xml: { text: "Export XML" },
+        importXML: {
+          text: "Import XML",
+          icon: FamilyTree.icon.xml(24, 24, "red"),
+          onClick: importXMLHandler,
+        },
+      },
+      nodeTreeMenu: true,
+      mouseScrool: FamilyTree.action.none,
+      nodeBinding: {
+        field_0: "name",
+      },
+    });
+    family.loadXML(familyData());
+  }
+
+  function importXMLHandler() {
+    family.importXML();
+  }
+})(window, document, undefined);
